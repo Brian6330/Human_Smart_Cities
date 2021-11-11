49894f13b1bc95cf074de2e8a36e19db235a90fc
Brian6330 <games@schweigler.org>
2021-11-11 15:25:59 +0100

    pdf_uploader updated to add per id

--- scripts/pdf_uploader.py
+++ scripts/pdf_uploader.py
@@ -30,10 +30,13 @@ solr = pysolr.Solr("http://localhost:8983/solr/pdf-uploader/", always_commit=Tru
 ###
 
 # TODO get the following working to POST pdf files
-f = open("../Data/asrbradiationcodeportingdocumentationpriscofreifinal.pdf", mode='rb')
+f = open("../Data/PERMOS_DataPolicy.pdf", mode='rb')
 # f_bi = open("Data/asrbradiationcodeportingdocumentationpriscofreifinal.pdf", mode='rb', encoding='utf-8', errors='ignore')
 
-solr.extract(file_obj=f, extractOnly=False, handler="update/extract", kwargs="literal.id=678")
+temp_list = {"literal.id": "699"}
+
+solr.extract(file_obj=f, extractOnly=False, handler="update/extract", **temp_list)
+
 
 
 results = solr.search('*') # Can be replaced by what you one
