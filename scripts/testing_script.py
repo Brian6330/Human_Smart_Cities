c2f91ed9f8f54b5a9ef926b81f081e42e41a9383
Brian6330 <brian@schweigler.org>
2021-12-02 16:18:09 +0100

    Experts: Skip empty author

--- scripts/testing_script.py
+++ scripts/testing_script.py
@@ -15,6 +15,9 @@ print('Extracted {} unique authors.'.format(len(set(authors))))
 # Get unique authors
 authors_with_keywords = []
 for current_author in set(authors):
+    if not current_author:
+        continue
+
     # Iterate through all results
     combined_dict = {}
     for result in search_results:
@@ -27,7 +30,7 @@ for current_author in set(authors):
             combined_dict = text_cleaner.combine_dicts(combined_dict, occurrences)
     sorted_keywords = sorted(combined_dict.items(), key=lambda x: x[1], reverse=True)
 
-    authors_with_keywords.append({current_author: sorted_keywords[0:100]})
+    authors_with_keywords.append({current_author: sorted_keywords[0:49]})
     print('Iterated through documents for {}'.format(current_author))
 
 print(authors_with_keywords)
