cc6f1e9c78369e6b8d592dc03c513a189821e8a2
Brian6330 <games@schweigler.org>
2021-12-04 12:04:38 +0100

    Evaluation: Work on evaluation.py, refactor testing_script to keyword_counter

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -1,7 +1,51 @@
+import pysolr
 
-def calc_precision(uuid, results, statistics):
-    true_positives = statistics['true_positives']  # TODO
-    false_positives = statistics['false_positives']  # TODO
+import keyword_counter
+
+solr = pysolr.Solr("http://localhost:8983/solr/hsc-data/", always_commit=True)
+
+search_results = solr.search('*')
+authors = keyword_counter.determine_authors(search_results)
+author_keyword_dict = keyword_counter.determine_keywords(search_results, authors)
+
+
+def calculate_statistics(search_results):
+    for current_author in authors:
+        file_titles = []
+        for result in search_results:
+            file_titles.append(search_results['title'])
+
+        keywords = []
+        for title in file_titles:
+            print("")
+
+        # true_positives = calc_true_positives(current_author, combined_dict, manual_dict)
+
+
+def calc_true_positives(author, combined_dict, manual_dict):
+    # TODO
+    print("")
+
+
+def calc_false_positives(author, combined_dict, manual_dict):
+    # TODO
+    print("")
+
+
+def calc_true_negatives(author, combined_dict, manual_dict):
+    # TODO
+    print("")
+
+
+def calc_false_positives(author, combined_dict, manual_dict):
+    # TODO
+    print("")
+
+
+
+
+
+def calc_precision(uuid, results, true_positives, false_positives):
 
     total_positives = true_positives + false_positives
 
@@ -15,9 +59,8 @@ def calc_precision(uuid, results, statistics):
         100 * precision
     ))
 
-def calc_recall(uuid, results, statistics):
-        true_positives = statistics['true_positives']  # TODO
-        false_negatives = statistics['false_negatives']  # TODO
+
+def calc_recall(uuid, results, true_positives, false_negatives):
 
         actual_positives = true_positives + false_negatives
         if actual_positives == 0:
---
+++ scripts/keyword_counter.py
@@ -0,0 +1,37 @@
+import text_cleaner
+
+
+def determine_authors(search_results):
+    # for id in search_results.:
+    authors = []
+    for result in search_results:
+        authors.append(result['author'])
+    print('Extracted {} unique authors.'.format(len(set(authors))))
+    return set(authors)
+
+
+def determine_keywords(search_results, authors):
+    # Get unique authors
+    authors_with_keywords = []
+    for current_author in authors:
+        if not current_author:
+            continue
+
+        # Iterate through all results
+        combined_dict = {}
+        for result in search_results:
+
+            # When matching current author: tokenize and count
+            if result['author'] is current_author:
+                tokenized_text = text_cleaner.clean_text(result['content'][0].lower())
+                occurrences = text_cleaner.create_count_set(tokenized_text)
+
+                combined_dict = text_cleaner.combine_dicts(combined_dict, occurrences)
+        sorted_keywords = sorted(combined_dict.items(), key=lambda x: x[1], reverse=True)
+
+        authors_with_keywords.append({current_author: sorted_keywords[0:49]})
+        print('Iterated through documents for {}'.format(current_author))
+
+    print('done')
+
+    return authors_with_keywords
