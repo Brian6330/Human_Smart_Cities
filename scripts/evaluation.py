65716650946c0f6ea84555b7fcd4121e9eda505e
Brian6330 <games@schweigler.org>
2021-12-10 11:47:15 +0100

    Expert: Determine true_positives, false_positives, false_negatives, and true_negatives by using all the terms that are in the manual_keyword_list

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -48,7 +48,7 @@ def search_for_keyword(author_keyword_dict, search_term="swiss", type_tuple=Fals
     if type_tuple:
         for current_dict in author_keyword_dict:
             for author in current_dict:
-                i = 0 # Keep track of the index, to know how "good" the match is
+                i = 0  # Keep track of the index, to know how "good" the match is
                 for term in current_dict.get(author):
                     if term[0] == search_term:
                         matching_authors.append(tuple([author, i]))
@@ -57,7 +57,7 @@ def search_for_keyword(author_keyword_dict, search_term="swiss", type_tuple=Fals
         sorted_matching_authors = sorted(matching_authors, key=lambda tup: tup[1])
         # Extract only the authors in order of best match
         sorted_matching_authors_only = [a_tuple[0] for a_tuple in sorted_matching_authors]
-        return set(sorted_matching_authors_only)
+        return list(set(sorted_matching_authors_only))
 
     # Manually extracted keywords just searches for matching author
     else:
@@ -66,7 +66,8 @@ def search_for_keyword(author_keyword_dict, search_term="swiss", type_tuple=Fals
                 for term in current_dict.get(author):
                     if term == search_term:
                         matching_authors.append(author)
-        return set(matching_authors)
+        # Remove duplicates and return the list
+        return list(set(matching_authors))
 
 
 def complete_manual_term_list(uploader_title_dict):
@@ -144,11 +145,46 @@ def calc_recall(uuid, results, true_positives, false_negatives):
     ))
 
 
+def evaluate_matches(manual_keyword_author_dict, automatic_author_keyword_dict, complete_manual_term_list):
+    author_choice_tp = 0
+    author_choice_tn = 0
+    author_choice_fp = 0
+    author_choice_fn = 0
+
+    for _ in range(50):
+        selected_word = random.choice(complete_manual_term_list)
+        manual_matches = search_for_keyword(manual_keyword_author_dict, selected_word)
+        automatic_matches = search_for_keyword(automatic_author_keyword_dict, selected_word, type_tuple=True)
+
+        # Both lists have matches and the first or second expert are matching
+        if manual_matches and automatic_matches:
+            if len(automatic_matches) >= 2:
+                if automatic_matches[0] in manual_matches or automatic_matches[1] in manual_matches:
+                    author_choice_tp += 1
+            elif automatic_matches in manual_matches:
+                author_choice_tp += 1
+
+        # If no matching results in manual list, but matching in automatic -> false positive
+        elif not manual_matches and automatic_matches:
+            author_choice_fp += 1 #TODO Never entered
+
+        # Manual matches but no automatic matches -> false negative
+        elif manual_matches and not automatic_matches:
+            author_choice_fn += 1
+
+        # Both lists return no matches -> true negative
+        elif not manual_matches and not automatic_matches:
+            author_choice_tn += 1 #TODO Never entered
+
+    return author_choice_tp, author_choice_fp, author_choice_fn, author_choice_tn
+
+
 manual_keyword_author_dict = combine_manual_keywords(uploader_title_dict)
 complete_manual_term_list = complete_manual_term_list(uploader_title_dict)
+tp, fp, fn, tn = evaluate_matches(manual_keyword_author_dict, automatic_author_keyword_dict, complete_manual_term_list)
+print("True positives: {}; False Positives: {}; False Negatives: {}; True Negatives: {}".format(tp, fp, fn, tn))
 
-# TODO Challenger
-
+# TODO Use all words?
 # TODO Statistics
 
 print(search_for_keyword(manual_keyword_author_dict, "data"))
