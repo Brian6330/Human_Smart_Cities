f59dc54c26efa90cc8f04f0d72b0f3e0a67e1fca
Julius Oeftiger <>
2021-12-10 14:44:35 +0100

    move gui code to scripts/

--- scripts/gui/gui.py
+++ scripts/gui/gui.py
@@ -1,11 +1,14 @@
-from PySide6.QtWidgets import *
+import sys
+from typing import AnyStr
+
 from PySide6.QtCore import Qt
+from PySide6.QtWidgets import *
 
 from solr import *
 
 
-class Result(QListWidgetItem):
-    def __init__(self, doc_id, title, author):
+class SearchResult(QListWidgetItem):
+    def __init__(self, doc_id: AnyStr, title: AnyStr, author: AnyStr):
         super().__init__()
 
         content = str(doc_id) + "\t" + str(title) + "\t" + str(author)
@@ -13,37 +16,38 @@ class Result(QListWidgetItem):
 
 
 class GUI(QWidget):
-
     def __init__(self):
         super().__init__()
 
-        server_label = QLabel('Server address:')
-        self.server_address = QLineEdit('http://localhost:8983/solr/hsc-data/')
+        ROW = 0
 
-        param_label = QLabel('Search parameters:')
+        layout = QGridLayout()
+
+        self.server_address = QLineEdit('http://localhost:8983/solr/hsc-data/')
+        layout.addWidget(QLabel('Server address:'), ROW, 0)
+        layout.addWidget(self.server_address, ROW, 1)
+        ROW += 1
+
         self.search_box = QLineEdit()
         self.search_btn = QPushButton('Search')
         self.search_btn.clicked.connect(self.search)
         self.fuzzy_toggle = QCheckBox('Fuzzy')
+        layout.addWidget(QLabel('Search parameters:'), 1, 0)
+        layout.addWidget(self.search_box, ROW, 1)
+        layout.addWidget(self.search_btn, ROW, 2)
+        layout.addWidget(self.fuzzy_toggle, ROW, 3)
+        ROW += 1
 
-        results_label = QLabel('Results:')
-        self.results = QListWidget()
-        self.results = QTableWidget()
-        self.results.setColumnCount(3)
-        self.results.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        self.results.setHorizontalHeaderLabels(['ID', 'Title(s)', 'Author(s)'])
+        self.search_results = QTableWidget()
+        self.search_results.setColumnCount(3)
+        self.search_results.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.search_results.setHorizontalHeaderLabels(['ID', 'Title(s)', 'Author(s)'])
+        layout.addWidget(QLabel('Results:'), ROW, 0)
+        ROW += 1
+        layout.addWidget(self.search_results, ROW, 0, 20, 4)
+        ROW += 1
 
-        layout = QGridLayout()
-        layout.addWidget(server_label, 0, 0)
-        layout.addWidget(self.server_address, 0, 1)
-
-        layout.addWidget(param_label, 1, 0)
-        layout.addWidget(self.search_box, 1, 1)
-        layout.addWidget(self.search_btn, 1, 2)
-        layout.addWidget(self.fuzzy_toggle, 1, 3)
-
-        layout.addWidget(results_label, 2, 0)
-        layout.addWidget(self.results, 3, 0, 32, 3)
+        self.experts = QTableWidget()
 
         self.setLayout(layout)
 
@@ -56,16 +60,16 @@ class GUI(QWidget):
 
         # unneeded as we overwrite cell contents after setting row count
         # self.results.clearContents()
-        self.results.setRowCount(len(docs))
+        self.search_results.setRowCount(len(docs))
         for row_index, doc in enumerate(docs):
             doc_id = QTableWidgetItem(str(doc['id']))
             doc_id.setFlags(doc_id.flags() & ~Qt.ItemIsEditable)
-            self.results.setItem(row_index, 0, doc_id)
+            self.search_results.setItem(row_index, 0, doc_id)
 
             title_str = ', '.join(doc.get('title', []))
             title = QTableWidgetItem(title_str)
             title.setFlags(title.flags() & ~Qt.ItemIsEditable)
-            self.results.setItem(row_index, 1, title)
+            self.search_results.setItem(row_index, 1, title)
 
             authors = doc.get('author', [])
             if authors is list:
@@ -74,6 +78,14 @@ class GUI(QWidget):
                 author_str = str(authors)
             author = QTableWidgetItem(author_str)
             author.setFlags(author.flags() & ~Qt.ItemIsEditable)
-            self.results.setItem(row_index, 2, author)
+            self.search_results.setItem(row_index, 2, author)
 
 
+if __name__ == "__main__":
+    app = QApplication([])
+
+    widget = GUI()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
